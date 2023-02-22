75e92f48cc9f3115f74991cf7fa2c69669ff0542
marisol varela <marisol.varela.cab@gmail.com>
2023-02-22 14:56:19 +0100

    flip-card

--- js/main.js
+++ js/main.js
@@ -12,13 +12,13 @@ previous.addEventListener('click', ()=>{
     removeChildNodes(pokemonContainer);
     serchPokemons(offset, limit);
    }
-})
+});
 
 next.addEventListener('click',()=>{
     offset += 9;
     removeChildNodes(pokemonContainer);
     serchPokemons(offset, limit);
-})
+});
 
 
 const fetchPokemon = (id) =>{
@@ -27,6 +27,7 @@ const fetchPokemon = (id) =>{
     .then(data => {
         createPokemon(data);
         spinner.style.display ='none';
+        console.log(data);
     });
 }
 
@@ -38,6 +39,14 @@ const serchPokemons = (offset, limit) =>{
 }
 
 const createPokemon = (pokemon) =>{
+    const flipCard = document.createElement('div');
+    flipCard.classList.add('flip-card');
+
+    const cardContainer= document.createElement('div');
+    cardContainer.classList.add('card-container');
+
+    flipCard.appendChild(cardContainer);
+
     const card = document.createElement('div');
     card.classList.add('pokemon-block');
 
@@ -60,7 +69,49 @@ const createPokemon = (pokemon) =>{
     card.appendChild(number);
     card.appendChild(name);
 
-    pokemonContainer.appendChild(card);
+    const cardBack = document.createElement('div');
+    cardBack.classList.add('pokemon-block-back');
+
+    cardBack.appendChild(progressBars(pokemon.stats));
+
+    cardContainer.appendChild(card);
+    cardContainer.appendChild(cardBack);
+    pokemonContainer.appendChild(flipCard);
+}
+
+const progressBars = (stats)=>{
+    const statsContainer = document.createElement('div');
+    statsContainer.classList.add('stats-container');
+
+    for (let i = 0; i< 3; i++){
+       const stat = stats[i];
+
+       const statPercent = stat.base_stat / 2 + "%";
+       const statContainer = document.createElement('div');
+       statContainer.classList.add('stat-container');
+
+       const statName = document.createElement('p');
+       statName.textContent = stat.stat.name;
+
+       const progress = document.createElement('div');
+       progress.classList.add('progress');
+
+       const progressBar = document.createElement('div');
+       progressBar.classList.add('progress-bar');
+       progressBar.setAttribute('aria-valuenow', stat.base_stat);
+       progressBar.setAttribute('aria-valuemin', 0);
+       progressBar.setAttribute('aria-valuemax', 200);
+       progressBar.style.width = statPercent;
+
+       progressBar.textContent = stat.base_stat + '%';
+
+       progress.appendChild(progressBar);
+       statContainer.appendChild(statName);
+       statContainer.appendChild(progress);
+
+       statsContainer.appendChild(statContainer);
+    }
+    return statsContainer;
 }
 
 const removeChildNodes = (parent)=>{
